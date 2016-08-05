88d0c7f423b71cd68cf05a83e299bf4f5d0f0c89
aoLian <aoLian@NanJingOrient.local>
2016-08-06 03:38:45 +0800

    测试

---
+++ testGit/testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by aoLian on 16/8/6.
+//  Copyright © 2016年 www.orient-info.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by aoLian on 16/8/6.
+//  Copyright © 2016年 www.orient-info.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
